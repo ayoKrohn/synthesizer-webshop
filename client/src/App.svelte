8624b26ea3fb93fd56f4745a23d854916a1cacfe
Ayo <ayokrohn@gmail.com>
2022-05-23 12:25:56 +0200

    added changes to App.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,25 +1,18 @@
 <script>
-import { Router, Route, Link } from "svelte-navigator";
-import Navbar from "./components/navbar/Navbar.svelte";
-import Home from "./pages/home/Home.svelte";
-import Synths from "./pages/synths/Synths.svelte";
-import Drummachines from "./pages/drummachines/drummachines.svelte";
-import About from "./pages/about/About.svelte";
-import Login from "./pages/login/Login.svelte";
-
-
-</script>
-
-<Navbar/>
-
-<Router>
-		<Route path="/" component={Home}></Route>
-        <Route path="/synths" component={Synths}></Route>
-		<Route path="/drummachines" component={Drummachines}></Route>
-		<Route path="/about" component={About}></Route>
-		<Route path="/login" component={Login}></Route> 
-</Router>
-
-<style>
-	
-</style>
+    import { Router, Route, Link } from "svelte-navigator";
+    import Navbar from "./components/navbar/Navbar.svelte";
+    import { SvelteToast } from '@zerodevx/svelte-toast';
+    
+    const options = {
+        theme: {
+          "--toastBackground": "#48BB78"
+    },
+    };
+    
+    </script>
+    
+    <Navbar/>
+    <SvelteToast {options} />
+    <style>
+        
+    </style>
