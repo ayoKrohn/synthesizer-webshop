4f92068e77314df7160303f31baad097f7b75e2e
Ayo <ayokrohn@gmail.com>
2022-05-23 12:47:53 +0200

    added Product component

--- client/src/components/product/Product.svelte
+++ client/src/components/product/Product.svelte
@@ -1,34 +1,36 @@
 <script>
-import { onMount } from "svelte";
-import { synthURL } from "../../store/productData.js";
-
-export let products = [];
-
-onMount(async () => {
-    const res = await fetch(`http://localhost:3000/api/synths`);
-    const { data: productArray } = await res.json();
-    products = productArray;
-});
-
-function addToCart(){
-
-}
-
-</script>
-
-<div class="products">
-	{#each products as product}
-    <tr>
-        <td>{product.productName}</td>
-        <td>Price: {product.price}</td>
-        <td>{product.description}</td>
-        <td><button class="addToCart" on:click={addToCart}> Buy </button></td>
-    </tr>
-    {/each}
-</div>
-
-<style>
-    * {
-    border : solid 1px pink;
-    }
-</style>
+    import { onMount } from "svelte";
+    import { synthURL, cartItems, amountOfItems } from "../../store/productData.js";
+    
+    export let products = [];
+    
+    onMount(async () => {
+        const res = await fetch(synthURL);
+        const { data: productArray } = await res.json();
+        products = productArray;
+    });
+    
+    
+    const addToCart = (product) => {
+            cartItems.update(items => [...items, product]);
+            }
+     
+    
+    </script>
+    
+    <div class="products">
+        {#each products as product}
+        <tr>
+            <td>{product.productName}</td>
+            <td>Price: {product.price}</td>
+            <td>{product.description}</td>
+            <td><button class="addToCart" on:click={() => addToCart(product)}> Buy </button></td>
+        </tr>
+        {/each}
+    </div>
+    
+    <style>
+        * {
+        border : solid 1px pink;
+        }
+    </style>
